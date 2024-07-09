496136120beb529daa270e5165d84ac7df23f279
Rurouni98 <byungju98@gmail.com>
2024-07-09 18:03:36 +0900

    24.07.09 강의 후 업데이트

---
+++ _05_Spring_DI_Constructor/src/main/java/com/bit/springboard/coupling/HyundaiCar.java
@@ -0,0 +1,62 @@
+package com.bit.springboard.coupling;
+
+public class HyundaiCar implements Car {
+    private CarAudio carAudio;
+    private String color;
+
+    public HyundaiCar() {
+        System.out.println("HyundaiCar 생성자1 호출");
+    }
+
+    public HyundaiCar(CarAudio carAudio) {
+        System.out.println("HyundaiCar 생성자2 호출");
+        this.carAudio = carAudio;
+    }
+    
+    public HyundaiCar(CarAudio carAudio, String color) {
+        System.out.println("HyundaiCar 생성자3 호출");
+        this.carAudio = carAudio;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void engineOn() {
+        System.out.println("HyundaiCar 시동을 건다.");
+    }
+
+    public void engineOff() {
+        System.out.println("HyundaiCar 시동을 끈다.");
+    }
+
+    public void speedUp() {
+        System.out.println("HyundaiCar 속도를 올린다.");
+    }
+
+    public void speedDown() {
+        System.out.println("HyundaiCar 속도를 낮춘다.");
+    }
+
+    public void initMethod() {
+        System.out.println("HyundaiCar 객체 생성");
+    }
+
+    public void destroyMethod() {
+        System.out.println("HyundaiCar 객체 삭제");
+    }
+
+    public void volumeUp() {
+        carAudio.volumeUp();
+    }
+
+    public void volumeDown() {
+        carAudio.volumeDown();
+    }
+
+}
